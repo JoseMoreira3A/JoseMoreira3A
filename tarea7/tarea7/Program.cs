6d2e18a6a18379cdb4e18607303082253edd85a8
JoseMoreira3A <56555389+JoseMoreira3A@users.noreply.github.com>
2019-10-14 11:40:48 -0500

    Add files via upload

---
+++ tarea7/tarea7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tarea7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el numero 100 (para mostrar dos columnas del 100 a 1 & 1 a 100: ");
+            int numero = 100;
+            numero = Convert.ToInt32(Console.ReadLine());
+            //numero = Convert.ToInt64(Console.ReadLine());
+
+            for (int i = 1; i <= numero; i++)
+            {
+                Console.Write(i);
+                Console.Write("  ");
+                Console.WriteLine((numero + 1) - i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
