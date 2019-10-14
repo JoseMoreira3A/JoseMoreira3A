d5391d90c59f0a27295dd08ef97b01de9a4e6c8b
JoseMoreira3A <56555389+JoseMoreira3A@users.noreply.github.com>
2019-10-14 11:41:52 -0500

    Add files via upload

---
+++ tarea9/tarea9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tarea9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un numero entero para realizar la tabla de multiplicar: ");
+            int multiplicar = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(" ");
+            for (int i = 1; i <= 15; i++)
+            {
+                Convert.ToInt32(multiplicar);
+                Console.WriteLine("{0} x {1} = {2}", multiplicar, i, multiplicar * i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
