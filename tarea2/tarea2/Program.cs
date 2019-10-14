1d09ccefa437a0a22014c8a38e1f09f107f58035
JoseMoreira3A <56555389+JoseMoreira3A@users.noreply.github.com>
2019-10-14 11:36:52 -0500

    Add files via upload

---
+++ tarea2/tarea2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tarea2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.WriteLine("Ingrese un numero para formar el cuadrado: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+
+            if (numero >= 0 && numero <= 100)
+            {
+                
+                for (int i = 0; i < numero; i++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+
+                
+                for (int i = 0; i < numero - 2; i++)
+                {
+                    Console.Write("*");
+                    for (int j = 0; j < numero - 2; j++)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.WriteLine("*");
+                }
+
+                
+                for (int i = 0; i < numero; i++)
+                {
+                    Console.Write("*");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error... El dato ingresado debe "
+                        + "estar entre 0 y 100");
+            }
+
+
+            Console.ReadKey();
+            
+        }
+    }
+}
