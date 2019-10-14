9d839f8230ba9ae9dd7ba19de751f45e83375c51
JoseMoreira3A <56555389+JoseMoreira3A@users.noreply.github.com>
2019-10-14 11:39:02 -0500

    Add files via upload

---
+++ tarea4/tarea4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tarea4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char respuesta;
+            int horas = 0;
+            int total_pagar = 0;
+            //int precio_extra = 22;
+
+            //int horas, salario;
+
+            do
+            {
+                Console.Write("Horas trabajadas: ");
+                horas = Convert.ToInt32(Console.ReadLine());
+
+                if (horas <= 35)
+                {
+                    total_pagar = horas * 15;
+                    
+                }
+                else
+                {
+                    total_pagar = ((35 * 15) + (horas - 35) * 22);
+                    
+                }
+                Console.WriteLine("Cantidad a pagar: " + total_pagar);
+                Console.WriteLine("Desea calcular otro salario: (s/n)");
+                respuesta = Convert.ToChar(Console.ReadLine());
+            } while (respuesta != 's' && respuesta != 'S');
+        }
+    }
+}
