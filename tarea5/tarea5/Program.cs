2234fcac95569c8130b800b3e0f9d3beff7e7c2a
JoseMoreira3A <56555389+JoseMoreira3A@users.noreply.github.com>
2019-10-14 11:39:35 -0500

    Add files via upload

---
+++ tarea5/tarea5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tarea5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un numero entero, para el tamaño de la piramide: ");
+            int valor;
+
+            valor = Convert.ToInt32(Console.ReadLine());
+
+            for (int i=0; i <= valor; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                for (int k = 0; k < i; k++)
+                {
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
