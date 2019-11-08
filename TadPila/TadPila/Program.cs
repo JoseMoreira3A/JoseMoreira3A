9c3c83f9d9d5465899c72e86c975ce2f3b1f360c
JoseMoreira3A <56555389+JoseMoreira3A@users.noreply.github.com>
2019-11-07 21:24:34 -0500

    Add files via upload

---
+++ TadPila/TadPila/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TadPila
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Leer;
+
+            Stack TadPila = new Stack();
+
+            TadPila.Push("Uno");
+            TadPila.Push("Dos");
+            TadPila.Push("Tres");
+            TadPila.Push("Cuatro");
+
+            for (int i = 0; i < 4; i++)
+            {
+                Leer = (string)TadPila.Pop();
+                Console.WriteLine(Leer + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
