2c7b0eb33f881855f12025399fbab6d1f57bf8d9
JoseMoreira3A <56555389+JoseMoreira3A@users.noreply.github.com>
2019-11-07 21:14:12 -0500

    Add files via upload

---
+++ TadCola/TadCola/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TadCola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Leer;
+
+            Queue TadCola = new Queue();
+
+            TadCola.Enqueue("Uno");
+            TadCola.Enqueue("Dos");
+            TadCola.Enqueue("Tres");
+            TadCola.Enqueue("Cuatro");
+
+            for (int i = 0; i < 4; i++)
+            {
+                Leer = (string)TadCola.Dequeue();
+                Console.Write(Leer + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
